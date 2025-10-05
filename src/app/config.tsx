3b43a603b208b4ea236e75d447a44f25aa726da1
Rayann <121342455+Rayannnzn@users.noreply.github.com>
2025-10-05 15:26:06 +0500

    Update config.tsx

--- src/app/config.tsx
+++ src/app/config.tsx
@@ -4,6 +4,6 @@ import { anvil, zksync, sepolia } from 'wagmi/chains';
 export const config = getDefaultConfig({
   appName: 'CrowdFunding Dapp',
   projectId: '889178c840581c98577571f9af01e8c0', // You can get this from WalletConnect Cloud
-  chains: [anvil, sepolia],
+  chains: [sepolia, anvil,zksync],
   ssr: false,
 });
